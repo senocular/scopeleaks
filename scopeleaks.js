a06cb6b0ce50ffef1e8c539fd24708a243cfa475
Rui Lopes <ruidlopes@gmail.com>
2011-01-15 02:34:56 +0000

    fixed indentation

--- scopeleaks.js
+++ scopeleaks.js
@@ -1,46 +1,47 @@
 (function() {
+	var scope = 
+		typeof global !== "undefined" ? global :
+		typeof window !== "undefined" ? window :
+		this;
+
+	var original = undefined;
   
-  var scope = 
-    typeof global !== "undefined" ? global :
-    typeof window !== "undefined" ? window :
-    this;
+	var scopeleaks = { 
+		snapshot: function () {
+			var snapshot = new Object();
 
-  var original = undefined;
-  
-  var scopeleaks = { 
-    snapshot: function () {
-      var snapshot = new Object();
+			for (var i in scope)
+				snapshot[i] = 1;
 
-      for (var i in scope)
-        snapshot[i] = 1;
+			original = original || snapshot;
 
-      original = original || snapshot;
+			return snapshot;
+		},
+			
+		leaks: function (snapshot) {
+			var ss = snapshot || scopeleaks.snapshot();
+			var leaks = [];
 
-      return snapshot;
-    },
-    leaks: function (snapshot) {
-      var ss = snapshot || scopeleaks.snapshot();
-      var leaks = [];
-
-      for (var i in ss)
-        if (
-          !(scope.document && scope.document.getElementById(i) != null) &&
-          !(typeof(scope.opera) == 'object' && scope.opera.toString() == "[object Opera]" && i == "onhashchange") &&
-          !original[(i)])
-          leaks.push(i);
+			for (var i in ss)
+				if (
+					!(scope.document && scope.document.getElementById(i) != null) &&
+					!(typeof(scope.opera) == 'object' && scope.opera.toString() == "[object Opera]" && i == "onhashchange") &&
+					!original[(i)]
+				)
+					leaks.push(i);
     
-      return leaks;
-    }
-  };
+			return leaks;
+		}
+	};
   
-  if (typeof(window) !== 'undefined') {
-    window.scopeleaks = scopeleaks;
-  } else if (typeof(exports) !== 'undefined') {
-    exports.leaks = scopeleaks.leaks;
-    exports.snapshot = scopeleaks.snapshot;
-  }
+	if (typeof(window) !== 'undefined') {
+    	window.scopeleaks = scopeleaks;
+  	} else if (typeof(exports) !== 'undefined') {
+    	exports.leaks = scopeleaks.leaks;
+    	exports.snapshot = scopeleaks.snapshot;
+	}
   
-  scopeleaks.snapshot();
+	scopeleaks.snapshot();
   
-  return scopeleaks;
+	return scopeleaks;
 })();
